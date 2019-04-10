624c421eb97a442bb40b976594a6e466e6abf705
tungcachnhiet <49482067+tungcachnhiet@users.noreply.github.com>
2019-04-11 00:31:09 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://bdskhuvucquan12.blogspot.com'>https://bdskhuvucquan12.blogspot.com/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY NHÉ : <a href='https://www.facebook.com/C%C3%B4ng-ty-TNHH-C%C3%A1ch-Nhi%E1%BB%87t-Th%E1%BB%8Bnh-Ph%C3%A1t-788439951528242/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
